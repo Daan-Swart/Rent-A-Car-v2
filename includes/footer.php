9d26b8dac149b573d3ab0e0904315fa02bc0fa51
Daan Swart <daswart18@outlook.com>
2023-11-16 20:54:22 +0100

    added header and footer files

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<!-- Compiled and minified JavaScript -->
+<footer class="section center">
+    <p class="flow-text">SherryDesign &copy 2023 </p>
+</footer>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Materialize icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rent-A-Car</title>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo"><img src="img/logo.png" onmouseover="this.src='img/hover-logo.png';" onmouseout="this.src='img/logo.png';" alt=""></a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="#how-it-works">Hoe werkt het?</a></li>
+                        <li><a href="#car_rental">Auto huren</a></li>
+                        <li><a href="#van_rental">Busje huren</a></li>
+                        <li><a href="#how-it-works">Over ons</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="signin.php" class="waves-effect waves-light btn">Inloggen</a></li>
+                        <li><a href="registreren.php" class="waves-effect waves-light btn">Registreren</a></li>
+                    </ul>
+            </div>
+        </nav>
+    </div>
