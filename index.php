6c58b20cd1fa74ada7cb847bdaa65ac9ceb98fe4
Daan Swart <daswart18@outlook.com>
2023-11-16 20:40:21 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,213 @@
+<?php
+include("includes/header.php");
+?>
+
+<!-- Home -->
+<section class="slider">
+    <ul class="slides">
+        <li>
+            <img src="img/hero-img2.png">
+            <div class="caption center-align">
+                <h2>Welkom Bij <span>Rent-A-Car</span></h2>
+                <h5>De <span>nummer 1</span> autoverhuurder!</h5>
+            </div>
+        </li>
+    </ul>
+</section>
+
+<!-- Section: How-It-Works -->
+<section class="section section-hiw center scrollspy">
+    <div class="container">
+        <h4 class="center heading-title">Hoe Het Werkt</h4>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card-panel">
+                    <i class="material-icons large orange-text">date_range</i>
+                    <h4>Datum kiezen</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Tenetur, optio.</p>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card-panel">
+                    <i class="material-icons large orange-text">event_available</i>
+                    <h4>Reserveren</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Tenetur, optio.</p>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card-panel">
+                    <i class="material-icons large orange-text">euro_symbol</i>
+                    <h4>Afrekenen</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Tenetur, optio.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Section: About Us -->
+<section class="section section-about grey lighten-4" id="about">
+    <div class="container">
+        <h4 class="center heading-title">Over Ons</h4>
+        <div class="about-container">
+            <div class="about-img">
+                <img src="img/about.png" alt="">
+            </div>
+            <div class="about-text">
+                <span>In het kort...</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id maiores odio, dolor voluptatibus eveniet natus dolorem, aperiam architecto aut voluptate porro necessitatibus ipsa. Ullam eligendi animi delectus magni fugiat. Nesciunt deserunt quisquam soluta amet.</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt labore beatae eos alias iste, laboriosam quam provident in.</p>
+                <a href="#" class="btn">Lees meer</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Section: Cars: -->
+<section id="autos" class="section section-cars scrollspy">
+    <div class="container">
+        <h4 class="center heading-title">Populaire Auto's</h4>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/car1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>22-BX-AA</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/car1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>22-BX-AA</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/car1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>22-BX-AA</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="center">
+            <li><a href="#" class="waves-effect waves-light btn">Bekijk meer >>></a></li>
+        </div>
+    </div>
+</section>
+
+<!-- Section: Busjes: -->
+<section id="busjes" class="section section-cars scrollspy">
+    <div class="container">
+        <h4 class="center heading-title">Populaire Busjes</h4>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/van1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>24-AA-91</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/van1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>24-AA-91</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="img/van1.jpg" alt="">
+                    </div>
+                    <div class="card-content">
+                        <p>24-AA-91</p>
+                        <h3>Honda Civic</h3>
+                        <h2>€100<span>/per maand</span></h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="center">
+            <li><a href="#" class="waves-effect waves-light btn">Bekijk meer >>></a></li>
+        </div>
+    </div>
+</section>
+
+
+<!-- Section: Contact -->
+<section id="contact" class="section section-contact scrollspy">
+    <div class="container">
+        <h4 class="center heading-title">Contact</h4>
+        <div class="row">
+            <div class="col s12 m6">
+                <div class="card-panel center grey lighten-4">
+                    <i class="material-icons orange-text">email</i>
+                    <h5>Neem Contact Met Ons Op</h5>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Cupiditate ea rem nisi nihil odio doloribus harum numquam, dicta odit a.</p>
+                </div>
+                <ul class="collection with-header">
+                    <li class="collection-header">
+                        <h4>Pick-up point</h4>
+                    </li>
+                    <li class="collection-item">Rent-A-Car</li>
+                    <li class="collection-item" style="display: flex; align-items: center;"><i class="material-icons orange-text" style="margin-right: 10px">location_on</i>Lindelaan 1200, Almere</li>
+                    <li class="collection-item" style="display: flex; align-items: center;"><i class="material-icons orange-text" style="margin-right: 10px">phone</i>06-2222-22-22</li>
+                </ul>
+            </div>
+            <div class="col s12 m6">
+                <div class="card-panel grey lighten-3">
+                    <h5>Stuur ons een bericht</h5>
+                    <div class="input-field">
+                        <input type="text" placeholder="Naam">
+                    </div>
+                    <div class="input-field">
+                        <input type="text" placeholder="Email">
+                    </div>
+                    <div class="input-field">
+                        <input type="text" placeholder="Telefoonnumer">
+                    </div>
+                    <div class="input-field">
+                        <textarea class="materialize-textarea" placeholder="Type een bericht"></textarea>
+                    </div>
+                    <input class="btn" type="submit" value="Verzenden">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php
+include("includes/footer.php");
+?>
