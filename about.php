7f613f7f18d1cbc622c551a8573d6236510fe424
Daan Swart <daswart18@outlook.com>
2023-11-21 14:46:22 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php
+include("includes/header.php");
+?>
+
+<section class="section" id="about">
+    <h4 class="center">
+        <span>Over</span> Ons
+    </h4>
+    <div class="row">
+        <div class="col m12 l6">
+            <img src="img/about.png" width="100%" alt="car">
+        </div>
+        <div class="col m12 l6">
+            <span>De beste service...</span>
+            <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id maiores odio, dolor voluptatibus eveniet natus dolorem,
+                aperiam architecto aut voluptate porro necessitatibus ipsa. Ullam eligendi animi delectus magni fugiat. Nesciunt deserunt
+                quisquam soluta amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt labore beatae eos alias iste, laboriosam quam provident in.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deserunt natus ullam, modi obcaecati odio sequi itaque ducimus quam?
+                Doloribus perspiciatis explicabo necessitatibus praesentium reiciendis perferendis quia dolorum eligendi suscipit?
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id maiores odio, dolor voluptatibus eveniet natus dolorem,
+                aperiam architecto aut voluptate porro necessitatibus ipsa. Ullam eligendi animi delectus magni fugiat. Nesciunt deserunt
+                quisquam soluta amet.</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt labore beatae eos alias iste, laboriosam quam provident in.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deserunt natus ullam, modi obcaecati odio sequi itaque ducimus quam?
+                Doloribus perspiciatis explicabo necessitatibus praesentium reiciendis perferendis quia dolorum eligendi suscipit?
+            </p>
+            <a href="#" class="btn">Contact</a>
+        </div>
+    </div>
+</section>
+</section>
+
+<?php
+include("includes/footer.php");
+?>
