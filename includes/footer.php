6bee0e5d2596dcdb67d6ac85c14434673de6364a
Daan Swart <daswart18@outlook.com>
2023-11-21 14:46:45 +0100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!-- Compiled and minified JavaScript -->
 <footer class="section center">
-    <p class="flow-text">SherryDesign &copy 2023 </p>
+    <p class="flow-text" style="font-size: 1rem;">SherryDesign &copy 2023 </p>
 </footer>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
 <script src="js/materialize.js"></script>
