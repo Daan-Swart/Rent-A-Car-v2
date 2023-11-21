6dc103d3552e3de06a3e6a517ffa73aea66f0390
Daan Swart <daswart18@outlook.com>
2023-11-21 12:58:50 +0100

    Added about section

--- index.php
+++ index.php
@@ -16,6 +16,30 @@ include("includes/header.php");
     </ul>
 </section>
 
+<!-- Section: About -->
+<section id="about" class="section section-about scrollspy">
+    <div class="container">
+        <h4 class="center">
+            <span>Over</span> Ons
+        </h4>
+        <div class="row">
+            <div class="col s12 m6">
+                <img src="img/about.png" width="100%" alt="car">
+            </div>
+            <div class="col s12 m6">
+              
+                    <span>In het kort...</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia id maiores odio, dolor voluptatibus eveniet natus dolorem, aperiam architecto aut voluptate porro necessitatibus ipsa. Ullam eligendi animi delectus magni fugiat. Nesciunt deserunt quisquam soluta amet.</p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt labore beatae eos alias iste, laboriosam quam provident in.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non deserunt natus ullam, modi obcaecati odio sequi itaque ducimus quam? Doloribus perspiciatis explicabo necessitatibus praesentium reiciendis perferendis quia dolorum eligendi suscipit?
+                    </p>
+                    <a href="#" class="btn">Lees meer</a>
+
+            </div>
+        </div>
+    </div>
+</section>
+
 <!-- Section: Cars-->
 <section id="cars" class="section section-cars scrollspy">
     <div class="container">
