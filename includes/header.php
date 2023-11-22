b25950f8b4d1ba6a9b905efd9f61258039de8c3c
Daan Swart <daswart18@outlook.com>
2023-11-22 12:05:15 +0100

    Delete comment text

--- includes/header.php
+++ includes/header.php
@@ -52,24 +52,3 @@
             </div>
         </nav>
     </div>
-
-
-    <!-- <div class="navbar-fixed">
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo"><img src="img/logo.png" onmouseover="this.src='img/hover-logo.png';" onmouseout="this.src='img/logo.png';" alt=""></a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="#how-it-works">Hoe werkt het?</a></li>
-                        <li><a href="#car_rental">Auto huren</a></li>
-                        <li><a href="#van_rental">Busje huren</a></li>
-                        <li><a href="#how-it-works">Over ons</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li><a href="signin.php" class="waves-effect waves-light btn">Inloggen</a></li>
-                        <li><a href="registreren.php" class="waves-effect waves-light btn">Registreren</a></li>
-                    </ul>
-            </div>
-        </nav>
-    </div> -->
