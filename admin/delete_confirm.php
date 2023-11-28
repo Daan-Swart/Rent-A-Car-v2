c20e44b894743350a0d39a4e3d7668af39e19e02
Daan Swart <daswart18@outlook.com>
2023-11-28 12:52:05 +0100

    Create delete_confirm.php

---
+++ admin/delete_confirm.php
@@ -0,0 +1,19 @@
+<?php
+include("../includes/a_header.php");
+// include("includes/functions.inc.php");
+// require_once('includes/dbcon.inc.php');
+
+?>
+
+<div id="blur">
+</div>
+<div class="delete-confirm-box center">
+    <h3>Waarschuwing!</h3>
+    <p>Lorem ipsum dolor sit amet.</p><br>
+    <a href="#" class="btn green">Ja</a>
+    <a href="#" class="btn red">Nee</a>
+</div>
+
+<?php
+include("../includes/footer.php");
+?>
